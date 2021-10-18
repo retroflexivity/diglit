51c14c114d43d41b8969b58d51c51eaad8a66a5d
retroflexivity <49839603+retroflexivity@users.noreply.github.com>
2021-10-18 16:03:34 +0300

    .

--- parser.py
+++ parser.py
@@ -1,14 +1,19 @@
 import sys
 
-punctls = ".,!?;:—–1234567890"
+punctls = ".,!?;:—–1234567890IVX"
+endings = ["-й", "-ый", "-м", "-ым", "-го", "-ого", "-му", "-ому", "-ом"]
 
 doc = open(sys.argv[1], "r+")
 text = doc.read()
 
 for i in punctls:
     text = text.replace(i,"")
+for i in endings:
+    text = text.replace(i,"")
 text = text.replace("‑", "-")
+
 print(text)
 doc.seek(0)
 doc.write(text)
+doc.truncate()
 doc.close()
