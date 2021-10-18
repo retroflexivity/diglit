d01c275cc63d0e93418d1215ff15e26f40b360f1
retroflexivity <49839603+retroflexivity@users.noreply.github.com>
2021-10-18 16:42:55 +0300

    lemmatization commited

---
+++ lemmatizer.py
@@ -0,0 +1,16 @@
+from pymystem3 import Mystem
+import sys
+
+doc = open(sys.argv[1], "r")
+text = doc.read()
+doc.close
+
+m = Mystem()
+text = ''.join(m.lemmatize(text))
+
+print(text)
+output = open(sys.argv[2], "r+")
+output.seek(0)
+output.write(text)
+output.truncate()
+output.close()
