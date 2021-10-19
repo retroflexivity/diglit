dc2af7c41a945b5cf866941062afacec8e43674f
retroflexivity <49839603+retroflexivity@users.noreply.github.com>
2021-10-19 13:12:28 +0300

    parsed

--- parser.py
+++ parser.py
@@ -1,25 +1,57 @@
-import sys
+# import sys
 import re
 
-punctls = ".,!?;:—–1234567890"
-# endings = ["-й", "-ый", "-м", "-ым", "-го", "-ого", "-му", "-ому", "-ом"]
+punctls = '“”".,!?;—–&'
 
-doc = open(sys.argv[1], "r+")
-text = doc.read()
-doc.close()
+for year in range(2015, 2022):
+    for month in range(4):
+        if ((year == 2015) and (month < 2)):
+            continue
+        if ((year == 2021) and (month == 1)):
+            break
+        filename = str(year) + "_" + str(month)
+        # print(filename)
 
-text = re.sub('https://t.co/[0-z]*', "", text)
+        unpsd = open("trump_tw/unparsed/" + filename + ".txt", "r")
+        unps = unpsd.read()
+        unpsd.close()
 
-for i in punctls:
-    text = text.replace(i,"")
 
-# for i in endings:
-#     text = text.replace(i,"")
-# text = text.replace("‑", "-")
+        ps = re.sub('........t.co/[0-z]+', "", unps)
+        ps = ps.replace(' - ', " ")
+        ps = ps.replace("%", "percent")
+        ps = ps.replace(": ", " ")
+        ps = ps.replace(":\n", "\n")
 
-print(text)
-# output = open(sys.argv[2], "r+")
-# output.seek(0)
-# output.write(text)
-# output.truncate()
-# output.close()
+        usr = re.findall('@[&-z’]+', ps)
+        ps = re.sub('@[&-z’]+', "", ps)
+
+        hst = re.findall('#[&-z’]+', ps)
+        ps = re.sub('#[&-z’]+', "", ps)
+
+
+        for i in punctls:
+            ps = ps.replace(i,"")
+
+            if (filename == "2015_2"):
+                print(ps)
+
+        psd = open("trump_tw/cleansed/" + filename + ".txt", "r+")
+        psd.seek(0)
+        psd.write(ps)
+        psd.truncate()
+        psd.close()
+
+        usrd = open("trump_tw/hashtags/" + filename + ".txt", "r+")
+        usrd.seek(0)
+        for i in hst:
+            usrd.write(i + "\n")
+        usrd.truncate()
+        usrd.close()
+
+        hstd = open("trump_tw/users/" + filename + ".txt", "r+")
+        hstd.seek(0)
+        for i in hst:
+            hstd.write(i + "\n")
+        hstd.truncate()
+        hstd.close()
